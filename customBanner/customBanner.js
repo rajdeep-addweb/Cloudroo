a911d41a2eb57af6362d6792ccb3363eb4429a35
Lakshya Soni <lakshya.s@addwebsolution.in>
2025-03-28 16:50:03 +0530

    Delete customBanner directory

--- customBanner/customBanner.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-export default class CustomBanner extends LightningElement {
-    handleCallNow() {
-        window.location.href = 'tel:+1234567890';
-    }
-}
